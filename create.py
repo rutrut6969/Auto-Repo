328f7c2490e020c8dea9e647f741ade0176dbb0b
rutrut6969 <rutledgeisaac6969@gmail.com>
2019-06-16 21:15:52 -0400

    fixes

--- create.py
+++ create.py
@@ -1,6 +1,6 @@
 import sys
 import os
-from github import github
+from github import Github
 
 
 project = r"C:\Users\rutle\OneDrive\Documents\Code Projects"
@@ -10,7 +10,7 @@ git_pass = "Rutledge69"
 def create():
     folderName = str(sys.argv[1])
     os.makedirs(project + str(sys.argv[1]))
-    user = github(git_user, git_pass).get_user()
+    user = Github(git_user, git_pass).get_user()
     repo = user.create_repo(sys.argv[1])
     print("Succesfully created repository {}".format(sys.argv[1]))
 
